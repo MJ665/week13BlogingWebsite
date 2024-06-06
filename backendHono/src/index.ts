264c3bc9f8fbafd01853662a9f72ff098fc148bb
Meet Jain <meetj665@gmail.com>
2024-06-06 13:22:57 +0530

    weeek  13

--- backendHono/src/index.ts
+++ backendHono/src/index.ts
@@ -3,22 +3,24 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.post('/api/v1/user/signup', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('signupRoute!')
 })
 app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('signinRoute')
 })
 app.post('/api/v1/user/blog', (c) => {
-  return c.text('Hello Hono!')
+  const id = c.req.param
+  console.log (c)
+  return c.text('create blog!')
 })
 app.put('/api/v1/user/blog', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('edit blog!')
 })
 app.get('/api/v1/user/blog/:id', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('get blog!')
 })
 app.get('/api/v1/user/blog/bulk', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('get blogs!')
 })
 
 export default app
